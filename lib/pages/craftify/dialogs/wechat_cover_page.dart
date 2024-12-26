ac397d02fcc50fd9da8ff0878358f592bf2efd6c
Fei Liang <njliangfei@gmail.com>
2024-12-26 22:24:06 +0800

    add translations to all the n1 grammar pieces

---
+++ lib/pages/craftify/dialogs/wechat_cover_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WechatCoverPage extends StatefulWidget {
+  const WechatCoverPage({super.key});
+
+  @override
+  State<WechatCoverPage> createState() => _WechatCoverPageState();
+}
+
+class _WechatCoverPageState extends State<WechatCoverPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
