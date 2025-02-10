ffaa840ef0e763a67ecd63229a7f11c63cce8d3e
Fei Liang <njliangfei@gmail.com>
2025-02-10 17:22:41 +0800

    feat: vocabulary

--- lib/pages/vocabulary/bloc/vocabulary_state.dart
+++ lib/pages/vocabulary/bloc/vocabulary_state.dart
@@ -11,13 +11,33 @@ final class VocabularyLoading extends VocabularyState {}
 
 final class VocabularyLoaded extends VocabularyState {
   final Map<VocabularyType, List<VocabularyMenu>> type2Menus;
+
   final List<Word> wordList;
+  final String currentPath;
 
   const VocabularyLoaded({
     required this.type2Menus,
     required this.wordList,
+    required this.currentPath,
   });
 
+  VocabularyMenu getCurrentMenu() {
+    final parts = currentPath.split('/');
+    final category = VocabularyType.values.firstWhere(
+      (e) => e.name == parts[0],
+    );
+    final menu = type2Menus[category]!.firstWhere(
+      (e) => e.key == parts[1],
+    );
+    if (parts.length > 2) {
+      final subMenu = menu.subMenus.firstWhere(
+        (e) => e.key == parts[2],
+      );
+      return subMenu;
+    }
+    return menu;
+  }
+
   @override
-  List<Object> get props => [type2Menus, wordList];
+  List<Object> get props => [type2Menus, wordList, currentPath];
 }
--- lib/pages/vocabulary/vocabulary_word_list_page.dart
+++ lib/pages/vocabulary/vocabulary_word_list_page.dart
@@ -1,168 +1,79 @@
-import 'package:auto_size_text/auto_size_text.dart';
 import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:senluo_japanese_cms/pages/vocabulary/bloc/preview_bloc.dart';
-import 'package:senluo_japanese_cms/pages/vocabulary/views/vocabulary_preview_view.dart';
 
 import '../../common/models/word.dart';
 import '../../common/constants/colors.dart';
 import '../kanji/constants/styles.dart';
 
 class VocabularyWordListPage extends StatefulWidget {
+  final String title;
   final List<Word> wordList;
 
-  const VocabularyWordListPage({super.key, required this.wordList});
+  const VocabularyWordListPage({
+    super.key,
+    required this.title,
+    required this.wordList,
+  });
 
   @override
   State<VocabularyWordListPage> createState() => _VocabularyWordListPageState();
 }
 
 class _VocabularyWordListPageState extends State<VocabularyWordListPage> {
-  static final _kGroupColors = [
-    Colors.red[50],
-    Colors.orange[50],
-    Colors.yellow[50],
-    Colors.green[50],
-    Colors.cyan[50],
-    Colors.blue[50],
-    Colors.purple[50],
-    Colors.brown[50],
-  ];
-  List<String> _selectedGroupKeys = [];
+  late List<String> _groupKeys;
 
   @override
   Widget build(BuildContext context) {
-    final groupKeys =
+    _groupKeys =
         groupBy(widget.wordList, (word) => word.category).keys.toList();
-    return _buildContent(groupKeys);
+    return _buildContent();
   }
 
-  _buildContent(List<String> groupKeys) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Words'),
-      ),
-      body: Stack(
-        children: [
-          Column(
-            mainAxisSize: MainAxisSize.max,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              _buildTopFilter(groupKeys.toList()),
-              Expanded(
-                child: GridView.count(
-                  crossAxisCount: 3,
-                  childAspectRatio: 1,
-                  children: _selectedGroupKeys.isNotEmpty
-                      ? widget.wordList
-                          .where((word) =>
-                              _selectedGroupKeys.contains(word.category))
-                          .map<Widget>((word) => _buildWordCard(
-                                word,
-                                _kGroupColors[
-                                    groupKeys.indexOf(word.category)]!,
-                              ))
-                          .toList()
-                      : widget.wordList
-                          .map<Widget>((word) => _buildWordCard(
-                                word,
-                                _kGroupColors[
-                                    groupKeys.indexOf(word.category)]!,
-                              ))
-                          .toList(),
+  _buildContent() {
+    return DefaultTabController(
+      length: _groupKeys.length,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+          bottom: TabBar(
+            tabs: _groupKeys.map((key) => Tab(text: key)).toList(),
+          ),
+        ),
+        body: TabBarView(
+          children: _groupKeys
+              .map(
+                (key) => Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: _buildGroupContent(key),
                 ),
-              ),
-            ],
-          ),
-          Positioned.fill(
-            bottom: 32,
-            child: Align(
-              alignment: Alignment.bottomCenter,
-              child: _buildBottomActions(),
-            ),
-          ),
-        ],
+              )
+              .toList(),
+        ),
       ),
     );
   }
 
-  _buildTopFilter(List<String> groupKeys) {
-    return Wrap(
-      spacing: 8,
-      children: groupKeys.mapIndexed((index, key) {
-        return ChoiceChip(
-          label: Text(key),
-          selectedColor: _kGroupColors[index],
-          backgroundColor: _kGroupColors[index],
-          side: BorderSide.none,
-          selected: _selectedGroupKeys.contains(key),
-          onSelected: (selected) {
-            final selectedKeys = List<String>.from(_selectedGroupKeys);
-            if (selected) {
-              selectedKeys.add(key);
-            } else {
-              selectedKeys.remove(key);
-            }
+  Widget _buildGroupContent(String key) {
+    final words =
+        widget.wordList.where((word) => word.category == key).toList();
 
-            setState(() {
-              _selectedGroupKeys = selectedKeys;
-            });
-          },
+    return ListView.separated(
+      itemBuilder: (BuildContext context, int index) {
+        return ListTile(
+          title: Text(
+            words[index].text,
+            style: GoogleFonts.getFont(
+              kKanjiFontName,
+              fontSize: 16,
+              color: kBrandColor,
+            ),
+          ),
+          subtitle: Text(words[index].meaning.en),
         );
-      }).toList(),
-    );
-  }
-
-  _buildWordCard(Word word, Color color) => Card(
-        color: color,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            // Text(word.category),
-            AutoSizeText(
-              word.text,
-              style: GoogleFonts.getFont(
-                kKanjiFontName,
-                fontSize: 20,
-                color: kBrandColor,
-              ),
-            ),
-            Text(word.reading),
-          ],
-        ),
-      );
-
-  _buildBottomActions() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        ElevatedButton(
-          onPressed: () {
-            _showPreviewDialog(context);
-          },
-          child: const Text('Generate Image'),
-        )
-      ],
-    );
-  }
-
-  _showPreviewDialog(BuildContext context) {
-    return showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        content: SizedBox(
-          width: 800,
-          height: 640,
-          child: BlocProvider(
-            create: (context) =>
-                PreviewBloc()..add(PreviewStarted(words: widget.wordList)),
-            child: VocabularyPreviewView(),
-          ),
-        ),
-      ),
+      },
+      separatorBuilder: (_, __) => const Divider(height: 1),
+      itemCount: words.length,
     );
   }
 }
