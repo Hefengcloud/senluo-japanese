55635a63a79702edd666fd4021414deec170d475
Fei Liang <njliangfei@gmail.com>
2025-02-14 22:05:24 +0800

    feat: kana

--- lib/pages/kana/views/kana_piece_view.dart
+++ lib/pages/kana/views/kana_piece_view.dart
@@ -8,10 +8,16 @@ class KanaPieceView extends StatelessWidget {
   static const _kHiraganaFont = 'Zen Maru Gothic';
   static const _kKatakanaFont = 'Zen Kaku Gothic New';
 
-  static const _kanaFontSize = 20.0;
+  static const _kanaFontSize = 24.0;
 
   final Kana kana;
-  const KanaPieceView({super.key, required this.kana});
+  final KanaType type;
+
+  const KanaPieceView({
+    super.key,
+    required this.kana,
+    required this.type,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -20,36 +26,46 @@ class KanaPieceView extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
               const Gap(8),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Text(
-                    kana.hiragana,
-                    style: GoogleFonts.getFont(
-                      _kHiraganaFont,
-                      fontSize: _kanaFontSize,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Text(
-                    kana.katakana,
-                    style: GoogleFonts.getFont(
-                      _kKatakanaFont,
-                      fontSize: _kanaFontSize,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black54,
-                    ),
-                  ),
-                ],
-              ),
+              _buildKana(),
               const Gap(8),
-              Text(
-                kana.romaji,
-                style: const TextStyle(fontSize: 20, color: Colors.black54),
-              ),
+              _buildRomaji(),
               const Gap(8),
             ],
           )
         : const SizedBox.shrink();
+  }
+
+  Text _buildRomaji() {
+    return Text(
+      kana.romaji,
+      style: const TextStyle(fontSize: 20, color: Colors.black38),
+    );
+  }
+
+  Row _buildKana() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        if (type == KanaType.hiragana || type == KanaType.all)
+          Text(
+            kana.hiragana,
+            style: GoogleFonts.getFont(
+              _kHiraganaFont,
+              fontSize: _kanaFontSize,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        if (type == KanaType.katakana || type == KanaType.all)
+          Text(
+            kana.katakana,
+            style: GoogleFonts.getFont(
+              _kKatakanaFont,
+              fontSize: _kanaFontSize,
+              fontWeight: FontWeight.bold,
+              color: type == KanaType.all ? Colors.black54 : Colors.black,
+            ),
+          ),
+      ],
+    );
   }
 }
