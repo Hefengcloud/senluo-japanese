8945cfb7f8debe4bd1e3dc6630a4c5d3e5d189af
Fei Liang <njliangfei@gmail.com>
2025-02-25 00:05:26 +0800

    feat: home

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:senluo_japanese_cms/common/constants/themes.dart';
-import 'package:senluo_japanese_cms/pages/home/helpers/navigation_helper.dart';
-import 'package:senluo_japanese_cms/pages/home/helpers/navigation_item.dart';
+import 'package:senluo_japanese_cms/pages/business/business_page.dart';
+import 'package:senluo_japanese_cms/pages/business/cashflow_page.dart';
+import 'package:senluo_japanese_cms/pages/business/leadership_page.dart';
+import 'package:senluo_japanese_cms/pages/business/marketing_page.dart';
+import 'package:senluo_japanese_cms/pages/business/overhead_operation_page.dart';
+import 'package:senluo_japanese_cms/pages/business/products_page.dart';
+import 'package:senluo_japanese_cms/pages/business/sales_page.dart';
+import 'package:senluo_japanese_cms/pages/kana/kana_home_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,73 +16,57 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _selectedIndex = 0;
-
-  final allEntries = buildNavigationItems();
-
   @override
   Widget build(BuildContext context) {
-    final moreEntries = allEntries.sublist(4);
-
-    final navItems = List<NavigationItem>.from(allEntries.getRange(0, 4))
-      ..add(
-        NavigationItem(
-          icon: const Icon(Icons.more_horiz_outlined),
-          selectedIcon: const Icon(Icons.more_horiz),
-          label: "更多",
-          page: _NavPage(moreEntries),
+    return DefaultTabController(
+      length: 6,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('SENLUO JAPANESE'),
+          bottom: TabBar(
+            isScrollable: true,
+            tabs: [
+              Tab(child: Text("Leadership")),
+              Tab(child: Text("Products")),
+              Tab(child: Text("Marketing")),
+              Tab(child: Text("Sales")),
+              Tab(child: Text("Overhead Operation")),
+              Tab(child: Text("Cashflow")),
+            ],
+          ),
         ),
-      );
-    return Scaffold(
-      body: navItems[_selectedIndex].page,
-      bottomNavigationBar: NavigationBar(
-        onDestinationSelected: (index) {
-          setState(() {
-            _selectedIndex = index;
-          });
-        },
-        selectedIndex: _selectedIndex,
-        destinations: navItems
-            .map<NavigationDestination>(
-              (e) => NavigationDestination(
-                icon: e.icon,
-                selectedIcon: e.selectedIcon,
-                label: e.label,
+        body: TabBarView(
+          children: [
+            LeadershipPage(),
+            ProductsPage(),
+            MarketingPage(),
+            SalesPage(),
+            OverheadOperationPage(),
+            CashflowPage(),
+          ],
+        ),
+        drawer: _buildDrawer(context),
+      ),
+    );
+  }
+
+  _buildDrawer(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: [
+          DrawerHeader(
+            child: Text('森罗日语'),
+          ),
+          ListTile(
+            title: Text('仮名'),
+            onTap: () => Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => KanaHomePage(),
               ),
-            )
-            .toList(),
+            ),
+          ),
+        ],
       ),
     );
-  }
-}
-
-class _NavPage extends StatelessWidget {
-  final List<NavigationItem> items;
-
-  const _NavPage(this.items);
-
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: ListView(
-          children: items
-              .map<ListTile>(
-                (e) => ListTile(
-                  leading: e.icon,
-                  title: Text(e.label, style: kHomeNavTextStyle),
-                  trailing: const Icon(Icons.arrow_right),
-                  onTap: () => _onNavigate(context, e),
-                ),
-              )
-              .toList(),
-        ),
-      ),
-    );
-  }
-
-  _onNavigate(BuildContext context, NavigationItem item) {
-    Navigator.of(context).push(MaterialPageRoute(builder: (_) => item.page));
   }
 }
